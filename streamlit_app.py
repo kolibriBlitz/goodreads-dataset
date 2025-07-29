e67c5084f9225500b60431fd5b8a60a6ae1a1c3e
kolibriBlitz <mcf7725@gmail.com>
2025-07-29 16:51:03 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,31 +34,32 @@ df['genres'] = df['genres'].str.split(',').apply(
     lambda lst: [genre.strip() for genre in lst])
 
 # Show a multiselect widget with the genres using `st.multiselect`.
-genres = st.multiselect("Genre", df['genres'].explode().unique(), [])
+genres = st.multiselect("Genre", df['genres'].explode().unique(), ["fantasy","young-adult","romance"])
 
 
 # Show a slider widget with the years using `st.slider`.
 pages = st.slider("Length", df["num_pages"].min(),
-                  df["num_pages"].max(),(0,2201))
+                  df["num_pages"].max(),(0,2201),help="Number of pages")
 
 # Show a slider with the number of stars
-stars = st.slider("Rating",0,5,(0,5))
+stars = st.slider("Rating",0.0,5.0,help="Average star rating")
 
 # Filter the dataframe based on the widget input and reshape it.
 df_filtered = df[
     df['genres'].apply(lambda g_list: any(genre in g_list for genre in genres)) &
-    df['num_pages'].between(pages[0], pages[1])
+    df['num_pages'].between(pages[0], pages[1]) & df['avg_rating'] >= stars
 ]
 
-
+# possible reshape of dataframe
 
 
 
 # Display the data as a table using `st.dataframe`.
 st.dataframe(
     df_filtered,
-    use_container_width=True,
-    column_config={"original_title": st.column_config.TextColumn("Title")},
+    use_container_width=True, column_order=("original_title","author","num_pages","avg_rating"),
+    column_config={"original_title": st.column_config.TextColumn("Title"), "author": st.column_config.TextColumn(
+        "Author"), "num_pages": st.column_config.TextColumn("Length"), "avg_rating": st.column_config.TextColumn("Average Rating")},
 )
 
 # Display the data as an  chart using 
