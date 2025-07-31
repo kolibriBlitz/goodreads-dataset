f4cf1113e4b09c7ab764d47aa39b2da6d6a6ab51
kolibriBlitz <mcf7725@gmail.com>
2025-07-31 14:22:06 +0000

    new page

---
+++ pages/2_🍀_Feeling_Lucky?.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import numpy as pt
+
+st.set_page_config('Feeling Lucky?','🍀')
+
+st.title('Surprise Me!')
+st.markdown('## Random Reading List Generator')
+st.sidebar.header("Feeling Lucky")
+
+st.markdown(
+    "This page allows you get a random assortment of books.")
+
+# Load the data from a CSV. We're caching this so it doesn't reload every time the app
+# reruns (e.g. if the user interacts with the widgets).
+
+
+@st.cache_data
+def load_data():
+    df1 = pd.read_csv("data/goodreads_works.csv", index_col='work_id')
+    # df2 = pd.read_csv("data/goodreads_reviews.csv")
+    return df1
+
+df=load_data()
+
+num_books=st.number_input('How many books do you want to read?',1,13525,int)
+
+def surprise_me(n):
+    '''
+    Function expects integer value for book list amount.
+    Returns a random sample of k amount from dataframe rows range. List is meant to be used to locate rows in full book_list dataframe.
+    '''
+    reading_list = random.sample(range(0, 13525), k=(n))
+    return reading_list
+
+books = surprise_me(num_books)
+temp = df.sort_values(by='total_reviews', ascending=False)
+
+st.write("Surpise! Happy reading")
+st.write({temp.iloc[books, lambda temp:[2,3,6,7]]})
