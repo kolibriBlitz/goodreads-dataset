cfea4f17118ece4f502a0732ca09dcba29effbf3
kolibriBlitz <mcf7725@gmail.com>
2025-09-14 08:51:44 -0600

    Update sidebar success message with arrow

--- 📚 Goodreads Book List.py
+++ 📚 Goodreads Book List.py
@@ -15,7 +15,7 @@ st.write(
 )
 
 st.sidebar.header("Goodreads Dataset")
-st.sidebar.success('Choose your adventure')
+st.sidebar.success('Choose your adventure ⇧')
 
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app
 # reruns (e.g. if the user interacts with the widgets).
